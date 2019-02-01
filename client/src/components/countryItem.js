5cd5b6a3bb42175e3b8752b15a6136ca033e427c
Tomek Kulczyński <kulczynski.t@gmail.com>
2019-02-01 20:18:39 +0100

    favicon changed

--- client/src/components/countryItem.js
+++ client/src/components/countryItem.js
@@ -4,10 +4,10 @@ export default ({ country: { name, capital, population, flag } }) => {
   return (
     <div className='card card-body bg-secondary mb-3'>
         <div className="row">
-            <div className="col-md-3 d-flex justify-content-center align-items-center flex-column" 
+            <div className="col-md-3 d-flex justify-content-center align-items-center flex-column"
                 style={{color:'white'}}>
                 <h4>{ name }</h4>
-                <img src={flag} alt={name} style={{width:200}}/>
+                <img src={flag} alt={name} style={{width:200, height:100}}/>
             </div>
             <div className="col-md-6 card card-body bg-primary">
                 <ul className="list-group">

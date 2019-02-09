ae43022051822a411a9b406604046b928878ee18
Tomek Kulczyński <kulczynski.t@gmail.com>
2019-02-09 21:33:23 +0100

    build app

---
+++ public/precache-manifest.d7906d9c249794e1a3daf0de6a7c5979.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e385e8db0d20a37ea9d4",
+    "url": "/static/js/main.e385e8db.chunk.js"
+  },
+  {
+    "revision": "833ec018abd41da00f75",
+    "url": "/static/js/1.833ec018.chunk.js"
+  },
+  {
+    "revision": "e385e8db0d20a37ea9d4",
+    "url": "/static/css/main.94bf554a.chunk.css"
+  },
+  {
+    "revision": "e495e19fe3276f9e54f22397d97a74c9",
+    "url": "/index.html"
+  }
+];
